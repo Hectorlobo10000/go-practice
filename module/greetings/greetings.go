d52fe4b98d285284e3982907e5fbc0f4d54aa831
Hectorlobo10000 <hector.lobo10000@gmail.com>
2022-01-13 23:02:41 -0600

    Finish... first steps

--- module/greetings/greetings.go
+++ module/greetings/greetings.go
@@ -7,26 +7,60 @@ import (
 	"time"
 )
 
+// hello returns a greeting for the named person
 func Hello(name string) (string, error) {
+
+	// if no name was given, return an erro with a message
 	if name == "" {
 		return "", errors.New("empty name")
 	}
 
+	// create a message using a random format.
 	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprint(randomFormat())
 
 	return message, nil
 }
 
+// hellos returns a map that asssociates each of the named people
+// with a greeting message
+func Hellos(names []string) (map[string]string, error){
+
+	// a map to associeate names with messages.
+	messages := make(map[string]string)
+	
+	// loop through the received slice of names, calling
+	// the hello funciton to get a message for each name
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+	// in the map, associate the retrieved message with
+	// the name
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+// init sets initial values for variable sused int the function
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+// randomformat returns one of a set of gretting messages. the returned
+// message is selecte a t random.
 func randomFormat() string {
+	// a slice of message formats
 	formats := []string {
 		"Hi, %v. Welcome",
 		"Great to see you, %v",
 		"Hail, %v! Well met!",
 	}
 
+	// return one of the message formats selected at random.
 	return formats[rand.Intn(len(formats))]
 }
--- module/hello/hello.go
+++ module/hello/hello.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 	"log"
 
- 	"example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
+	// set properties of the predefined logger, including
+	//the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
 	log.SetPrefix("grettings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Hector")
+	// a slice of names.
+	names := []string{"Gladys", "Smantha", "Darrin"}
+
+	// request gretting messages for the names
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	// if no error was returned, print the returned mp of 
+	// messges to the console
+	fmt.Println(messages)
 }
